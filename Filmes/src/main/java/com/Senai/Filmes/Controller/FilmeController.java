157943d4f45d6b471331df9183d5799d13954fcc
Gustavo Baister <gustavobaister@gmail.com>
2026-06-03 20:06:30 -0300

    Parando na aula para a aba FilmeController

--- Filmes/src/main/java/com/Senai/Filmes/Controller/FilmeController.java
+++ Filmes/src/main/java/com/Senai/Filmes/Controller/FilmeController.java
@@ -6,10 +6,9 @@ import com.Senai.Filmes.Service.FilmeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 import java.util.List;
 
@@ -30,4 +29,9 @@ public class FilmeController {
         return new ResponseEntity<>(filmes, HttpStatus.OK);
 
     }
+
+    @GetMapping("/{id}") // Esse é para ter outro get que precisa de id
+    public ResponseEntity<FilmeResponse> buscarPorId(PathVariable UUID){
+        return new ResponseEntity<>(UUID, HttpStatus.OK);
+    } 
 }

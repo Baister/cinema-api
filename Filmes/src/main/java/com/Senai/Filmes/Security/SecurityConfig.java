14290ca23d9075353ff56374b2cbfcccf53b8033
Gustavo Baister <gustavobaister@gmail.com>
2026-06-09 21:39:36 -0300

    refactor: trocando o SecurityConfig de repositório

--- Filmes/src/main/java/com/Senai/Filmes/Security/SecurityConfig.java
+++ Filmes/src/main/java/com/Senai/Filmes/Security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.Senai.Filmes.Config;
+package com.Senai.Filmes.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

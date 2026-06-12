0a54c4004ca0fd21aa94f6103360b0633d5dbb43
Gustavo Baister <gustavobaister@gmail.com>
2026-06-11 21:07:19 -0300

    fix: ajustando @component

--- Filmes/src/main/java/com/Senai/Filmes/Config/DataInitializer.java
+++ Filmes/src/main/java/com/Senai/Filmes/Config/DataInitializer.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataInitializer implements CommandLineRunner {
     @Autowired
     private IUsuarioRepository usuarioRepository;
